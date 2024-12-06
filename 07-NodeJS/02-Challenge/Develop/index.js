0f293d7be5744542542cfe68309f33d83e2dae26
alanaainez <alainez649@gmail.com>
2024-12-06 16:25:39 -0500

    git commit -m "improved functions and packages"

--- 07-NodeJS/02-Challenge/Develop/index.js
+++ 07-NodeJS/02-Challenge/Develop/index.js
@@ -1,18 +1,70 @@
 // TODO: Include packages needed for this application
 import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './generateMarkdown.js';
 // TODO: Create an array of questions for user input
 const questions = [
-    {    name: 'username',
-    title: 'project',
-    contact: 'email',        
-    }
+    {type: 'input',
+    name: 'title',
+    message: 'Enter the title of your project:',
+    },
+    
+    {type: 'input',
+    name: 'description',
+    message: 'Enter a description of your project:',
+    },
+    
+    {type: 'input',
+    name: 'installation',
+    message: 'Enter installation instructions:',
+    },
+    
+    {type: 'input',
+    name: 'usage',
+    message: 'Enter usage information:',
+    },
+    
+    {type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'Apache', 'GPL', 'None'],
+    },
+    
+    {type: 'input',
+    name: 'username',
+    message: 'Enter your GitHub username:',
+    },
+    
+    {type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+    },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {fs.writeFile(fileName, data, (err) => {
+    if (err) {
+        console.error(`Error writing to file ${fileName}:`, err);
+    } else {
+        console.log(`Successfully wrote to ${fileName}`);
+    }
+});
+}
 
 // TODO: Create a function to initialize app
-function init() {}
-
+//function init() {}
+async function init() {
+    try {
+      const answers = await inquirer.prompt(questions);
+  
+      // Generate the README markdown content
+      const markdownContent = generateMarkdown(answers);
+  
+      // Write the content to a README file
+      writeToFile('README.md', markdownContent);
+    } catch (error) {
+      console.error('Error during initialization:', error);
+    }
+  }
 // Function call to initialize app
 init();
--- 07-NodeJS/02-Challenge/Develop/utils/generateMarkdown.js
+++ 07-NodeJS/02-Challenge/Develop/utils/generateMarkdown.js
@@ -2,30 +2,58 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (!license) return '';
-  return `Badge ${license}`;
+  return `![License](https://img.shields.io/badge/license-${license}-blue.svg)`;
 }
 console.log(renderLicenseBadge(license));
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (!license) return '';
-  return `Link url: ${license}`;
+  const licenseLinks = {
+    MIT: 'https://opensource.org/licenses/MIT',
+    Apache: 'https://opensource.org/licenses/Apache-2.0',
+    GPL: 'https://www.gnu.org/licenses/gpl-3.0',
+  };
+  return licenseLinks[license] || '';
 }
 console.log(renderLicenseLink(license));
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (!license) return '';
-  return `License Section ${license}`;
+  return `## License\nThis project is licensed under the ${license} license.`;
 }
 console.log(renderLicenseSection(license));
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-    const licenseBadge = renderLicenseBadge(data.license);
-    const licenseLink = renderLicenseLink(data.license);
-    const licenseSection = renderLicenseSection(data.license);
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.license);
+  const licenseSection = renderLicenseSection(data.license);
 
-    return `# ${data.title}`;
+  return `# ${data.title}
+${licenseBadge}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+
+## Installation
+To install the necessary dependencies, run the following command:
+
+\`\`\`
+${data.installation}
+\`\`\`
+
+## Usage
+${data.usage}
+
+## License
+${licenseSection}
+For more information, see the [${data.license} License](${licenseLink}).
+`;
 }
 module.exports = generateMarkdown;
